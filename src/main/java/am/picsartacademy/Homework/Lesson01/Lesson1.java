0bbc9c953ec511ead7341fa2a2bd0d35ed2c56bf
Your Name <annahunanyan.davoo@gmail.com>
2025-05-23 12:40:11 +0400

    🎉 Սկսեցի նախագիծը՝ Lesson01 Java

---
+++ src/main/java/am/picsartacademy/Homework/Lesson01/Lesson1.java
@@ -0,0 +1,94 @@
+package am.picsartacademy.Homework.Lesson01;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+
+
+        try {
+            int $ = 4;
+            int a$ = 8;
+            int am = 11;
+            int ad = 10;
+            int ff = 99;
+            System.out.println(am);
+            System.out.println(4 * 99);
+
+            int x = 7, y = 5;
+            x++;
+            x++;
+            System.out.println(x);
+            ++x;
+            System.out.println(x);
+
+            System.out.println(x / y);
+            System.out.println(x * y);
+            System.out.println(x);
+            System.out.println(y % x);
+
+            x = 24;
+            x++;
+            x++;
+            System.out.println(x);
+            int c = 0;
+            y = 2;
+            x = 7;
+            x++;
+            y--;
+            c = y + x;
+            System.out.println(x);
+            System.out.println(y);
+            System.out.println(c);
+
+            boolean sd = x == 12;
+            System.out.println(sd);
+
+            System.out.println(x > 5);
+            System.out.println(x <= 5);
+            System.out.println(x++);
+
+            x = 47;
+            System.out.println(x % 6 == 00);
+
+            int[] a = new int[6];
+            a[0] = 1;
+            a[1] = 30;
+            a[2] = 17;
+            a[3] = -1;
+            a[4] = 3;
+            System.out.println(a[0] + a[4]);
+            int[] array = {3, 4, 22, 51, -1, 63};
+            System.out.println(array[array.length - 1]);
+
+            int bb = 5;
+
+            if (bb > 3) {
+                System.out.println("ayooooooooooo");
+            } else {
+                System.out.println("vochhhhhhhhhh");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+        int tiv = 4;
+        switch (tiv) {
+            case 1:
+                System.out.println("N1");
+                break;
+            case 2:
+                System.out.println("N2");
+                break;
+            case 3:
+                System.out.println("N3");
+                break;
+            case 4:
+            default:
+                System.out.println("hamarakalum");
+
+
+                int[] anul = {6, 68, 89, 98, 65};
+        }
+
+    }
+}
